82e9ff274eefe052ee146600dfe89e344cccb260
Тимур <63254053+Timur1414@users.noreply.github.com>
2024-05-25 22:31:03 +0300

    Update tests.cpp

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,1 +1,31 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+class Prototype{
+public:
+	virtual ~Prototype(){
+
+	}
+	virtual Prototype* Clone() const = 0;
+protected:
+	Prototype(){
+
+	}
+};
+class ConcretePrototype:public Prototype{
+public:
+	ConcretePrototype(){
+
+	}
+	ConcretePrototype(const ConcretePrototype& cp){
+		*this = cp;	
+		cout<<"ConcretePrototype copy ..."<<endl;
+	}
+	~ConcretePrototype(){
+
+	}
+	virtual Prototype* Clone() const{
+		return new ConcretePrototype(*this);
+	}
+};
 
